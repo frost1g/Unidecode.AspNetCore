6415fc78b80d44b075ca65ffbb84189a32d7a60c
Michael <michael.baranchikov@hotmail.com>
2018-12-06 14:28:58 +0300

    initialization

---
+++ Unidecode.AspNetCore.Tests/Unicoder.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Unidecode.AspNetCore.Tests
+{
+    [TestFixture]
+    public class Unicoder
+    {
+        [Test]
+        public void UnidecodeTest()
+        {
+            Assert.AreEqual("Bei Jing ", "\u5317\u4EB0".Unidecode());
+            Assert.AreEqual("Rabota s kirillitsey", "Работа с кириллицей".Unidecode());
+            Assert.AreEqual("Hello, World!", "Hello, World!".Unidecode());
+            Assert.AreEqual("'\"\r\n", "'\"\r\n".Unidecode());
+            Assert.AreEqual("CZSczs", "ČŽŠčžš".Unidecode());
+            Assert.AreEqual("a", "ア".Unidecode());
+            Assert.AreEqual("a", "α".Unidecode());
+            Assert.AreEqual("a", "а".Unidecode());
+            Assert.AreEqual("chateau", "ch\u00e2teau".Unidecode());
+            Assert.AreEqual("vinedos", "vi\u00f1edos".Unidecode());
+        }
+    }
+}
